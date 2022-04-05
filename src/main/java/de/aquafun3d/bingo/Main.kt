8e68aaf43d4117a79b2073970d386a0f2b03208b
AquaFun3D <nino@theska.de>
2022-04-05 15:21:08 +0200

    Revert "Changed player messages"
    This reverts commit 86b112181a352db4602d71cf521196f6a62a00c6.

---
+++ src/main/java/de/aquafun3d/bingo/Main.kt
@@ -0,0 +1,13 @@
+package de.aquafun3d.bingo
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+	override fun onEnable() {
+		// Plugin startup logic
+	}
+
+	override fun onDisable() {
+		// Plugin shutdown logic
+	}
+}
